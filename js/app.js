fe8ab10443b04363c5cf6fd99029c0a1cf8d1fd9
Nick Chang <nick.chang@edge-technologies.com>
2016-12-18 17:06:14 -0500

    added  support

--- js/app.js
+++ js/app.js
@@ -1,51 +1,75 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngCookies']);
 
-angular.module('app').controller('MainCtrl', function ($scope, $compile) {
+app.controller('MainCtrl', function ($scope, $compile, $cookies) {
 	
-	$scope.add = function(ev,attrs){
-	  
-	  var tpl = angular.element('<bell></bell>');
-	  var newScope = $scope.$new(true);
 
-	  $compile(tpl)(newScope);
-      //$scope.insertHere = el;
-	  $("#reminderBell").append(tpl);
+	$scope.add = function(bellObj) {
+	  	//console.log(bellObj);
+	  	var m = bellObj;
+	  	if (m == null) {
+		 	var date = new Date();
+			var hours = date.getHours();
+			var ampm = hours >= 12 ? 'pm' : 'am';
+			hours = hours % 12;
+			hours = hours ? hours : 12; // the hour '0' should be '12'
+
+			m = {
+				bellType: "big",
+				second:  date.getSeconds(),
+				minute:  date.getMinutes(),
+				hour: hours,
+				ap: ampm
+			}
+		}
+
+		var tpl = angular.element('<bell></bell>');
+		var newScope = $scope.$new(true);
+		newScope.m = m;
+
+		$compile(tpl)(newScope);
+		//$scope.insertHere = el;
+		$("#reminderBell").append(tpl);
     };
 
     $scope.delete = function(ev,attrs){//$on('insertItem',function(ev,attrs){
 		if ($("#reminderBell").children().length > 1) {
 			$("#reminderBell").children().last().remove();
 		}
-		//$( ".reminderBell" ).remove();
+		$scope.persist();
     };
+
+	$scope.persist = function () {
+		var array = [];
+		$("#reminderBell").children().each(function(){
+    		var s = angular.element(this).scope();
+    		array.push(s.m);
+		});
+		$cookies.putObject("reminderBell",array);
+	}
+
+    var bells = $cookies.getObject("reminderBell");
+    if (bells != null) {
+	    bells.forEach(function(obj){
+	    	if (obj != null) {
+	    		//console.log(obj);
+	    		$scope.add(obj);
+	    	}
+	    })
+	}
+
 });
 
 
 // directive
-angular.module('app')
-  .directive('bell', function () {
+app.directive('bell', function () {
     return {
      	templateUrl: 'js/reminderBell.tpl.html',
       	restrict: 'E',
-		replace: "true"
+		replace: true
     };
   });
   
-angular.module('app').controller('bellCtrl', function ($scope, $timeout) {
-	$scope.bellType = "big";
-	
-	var date = new Date();
-	var hours = date.getHours();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-	
-	$scope.m = {
-		second:  date.getSeconds(),
-		minute:  date.getMinutes(),
-		hour: hours,
-		ap: ampm
-	}
+app.controller('bellCtrl', function ($scope, $timeout, $cookies) {
 	
 	$scope.timerID=null;
 	$scope.disabled = false;
@@ -82,14 +106,14 @@ angular.module('app').controller('bellCtrl', function ($scope, $timeout) {
 	    var now = new Date();
 	    var diff = d.getTime() - now.getTime();
 	    if (diff <= 0) {
-	        alert("Please input a future time.");
-	        return;
+	        alert("You enter a tomorrow's time.");
+	        diff += 24*60*60*1000;
 	    }
 	
 		$scope.disabled = true;
 	
 		$scope.timerID = $timeout(function(){
-			if ($scope.bellType === "big") {
+			if ($scope.m.bellType === "big") {
 		 	  	document.getElementById('G_BigID').play();
 			} else {
 				document.getElementById('G_SmallID').play();
@@ -98,12 +122,16 @@ angular.module('app').controller('bellCtrl', function ($scope, $timeout) {
 			$scope.disabled = false;	
 		}, diff);
 
-	    //setStatus("status_reminder","In Progress");
-		/*settings.time_HH = $("#time_HH").val();
-		settings.time_MM = $("#time_MM").val();
-		settings.time_SS = $("#time_SS").val();
-		persistSettings();*/
+		$scope.persist();
 	}
 	
+	$scope.persist = function () {
+		var array = [];
+		$("#reminderBell").children().each(function(){
+    		var s = angular.element(this).scope();
+    		array.push(s.m);
+		});
+		$cookies.putObject("reminderBell",array);
+	}
 	
   });
